33d53bbe114ae4bf08738b673a9163055f1d8dd3
Bablu <bablu.chilukoti@gmail.com>
2023-10-19 20:54:34 +0530

    updated color code syntax echo -e "\e[codemMessage\e[0m"

--- print.sh
+++ print.sh
@@ -1,1 +1,16 @@
 echo Hello world!
+
+#Red     31
+#Green   32
+#Yellow  33
+#Blue    34
+#Magenta 35
+#cyan    36
+
+#syntax
+#echo -e "\e[codemMESSAGE\e[0m"
+
+echo -e "\e[34mHello world\e[0m"
+echo -e "\e[32mHello world\e[0m"
+echo -e "\e[33mHello World\e[0m"
+echo -e "\e[34Hello World\e[0m"
